bef3c6cdf6cfdd676236ec4fb40b6fb10e82b075
madjestic <madjestic13@gmail.com>
2014-02-10 22:38:05 +0100

    foobar

--- TinyMath.hs
+++ TinyMath.hs
@@ -1,4 +1,4 @@
-module TinyMath where
+module TinyMath.TinyMath where
 
 -- |
 -- | Prime Factorisation
@@ -6,14 +6,17 @@ module TinyMath where
 
 -- | isPrime1 n k is just an interface function for isPrime
 isPrime1 :: RealFrac a => a -> Bool
-isPrime1 n = isPrime n 2
+isPrime1 n 
+         | n == 1    = False
+         | n == 2    = True
+         | otherwise = isPrime n 2
 
 -- | isPrime needs an index k (since we can't use variables)
 isPrime :: RealFrac a => a -> a -> Bool
 isPrime n k
         | k < 2                                    = error "k must be >= 2"
-        | n /= k && (n `isDivisableBy` k) == True  = False
-        | n > k && (n `isDivisableBy` k)  == False = isPrime n (k+1)
+        | n /= k &&     (n `isDivisableBy` k)      = False
+        | n  > k && not (n `isDivisableBy` k)      = isPrime n (k+1)
         | n == k                                   = True
 
 
@@ -62,28 +65,35 @@ instance Eq Direction where
          _ == _ = False
 
 
-offset :: (Eq t, Num t, Num t2, Num t1) => (t1, t2) -> (Direction, t) -> [(t1, t2)]
-offset _ (_,0) = []
-offset (x,y) (direction, steps)
-         | direction == Right' = stepRight : offset stepRight (Right',(steps - 1))
-         | direction == Up     = stepUp    : offset stepUp    (Up    ,(steps - 1))
-         | direction == Left'  = stepLeft  : offset stepLeft  (Left' ,(steps - 1))
-         | direction == Down   = stepDown  : offset stepDown  (Down  ,(steps - 1))
-         | otherwise = error "foobar"
-                   where 
-                       stepRight = (x+1,y)
-                       stepUp    = (x,y+1)
-                       stepLeft  = (x-1,y)
-                       stepDown  = (x,y-1)
-                       
---traverse 
---  concat $ map (offset (0,0)) $ zip (directions $ double 2) (double 2)
 
+offset :: (Eq t, Num t, Num t1, Num t2) => (Direction, t) -> [(t1, t2)]
+offset (_,0) = []
+offset (direction, steps)
+         | direction == Right' =  (1,0) : offset (Right',(steps - 1))
+         | direction == Up     =  (0,1) : offset (Up    ,(steps - 1))
+         | direction == Left'  = (-1,0) : offset (Left' ,(steps - 1))
+         | direction == Down   = (0,-1) : offset (Down  ,(steps - 1))
+
+                       
+traverse :: Num a => a -> [a] -> [a]
+traverse _ []     = []
+traverse p (x:xs) = p + x : traverse (p + x) xs
+
+
+
+traverse1 :: (Num t, Num t1, Num t2) => t2 -> (t, t1) -> [(t, t1)] -> [(t, t1, t2)]
+traverse1 _ _ []     = []
+traverse1 k p (x:xs) = (\((a,b),c) -> (a,b,c)) (p + x, k) : traverse1 (k+1) (p + x) xs
+
+
+
+walkSpiral :: (Num t, Num t1, Num t2) => (t, t1) -> Int -> [(t, t1, t2)]
+walkSpiral p size = traverse1 1 p $ concat $ map offset $ zip (directions $ size') (size')
+           where
+              size' = double (size*2)
 
 instance (Num a, Num b) => Num (a, b) where
-         (x1,y1) + (x2,y2) = (x1+x2, y1+y2)
-         (x1,y1) * (x2,y2) = (x1*x2, y1*y2)
-
+         (a,b) + (c,d) = (a+c, b+d)
 
 
 directions :: [t] -> [Direction]
