416228c66d389ac5d36bf9a9a850a47e5a66e7d7
madiestic <madjestic13@gmail.com>
2024-12-09 11:54:28 +0100

    importing RealArithmetic

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,22 @@ module Main where
 import Data.List         (group)
 import Data.Set          (toList, fromList)
 import Control.Arrow
+import RealArithmetic
 
 f :: [Char] -> [(Int, [Char])]
 f x = zip (length <$> group x) (toSet <$> group x)
   where
     toSet = toList . fromList
 
+e' :: String -> Double
+e' = eval . parse . lexer
+-- | \> (e' "10 + 5") == (e' "5.5 + 9.5")
+-- | > True
+
 main :: IO ()
 main = do
   let foo = zip (length <$> group "aaaabbbcca") (toList . fromList <$> group "aaaabbbcca")
       bar = (\x -> zip (length <$> x) (toList . fromList <$> x)) $ group "aaaabbbcca"
       baz = (\ (x,y) -> zip x y ) $ (length <$>) &&& (toList . fromList <$>) $ group "aaaabbbcca"
-  putStrLn "Hello, Haskell!"
+  --putStrLn "Hello, Haskell!"
+  pure ()
